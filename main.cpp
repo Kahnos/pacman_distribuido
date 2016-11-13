ecec898c33d12a7506306be6140f149166af4833
kahnos <josed1305@gmail.com>
2016-11-13 17:56:58 -0400

    First commit.

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: kahnos
+ *
+ * Created on November 13, 2016, 5:54 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello Matrix";
+    return 0;
+}
+
